eb5072ebf746b0089728fe4c64d955d816fee0dd
Koldo Abasolo Iturregi <koldobikaabasolo@gmail.com>
2024-05-16 19:32:30 +0200

    BuenoBonitoBarato

---
+++ src/libfta/ft_strncmp.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strncmp.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/12/12 09:16:20 by kabasolo          #+#    #+#             */
+/*   Updated: 2024/04/24 12:58:36 by kabasolo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_strncmp(const char *s1, const char *s2, size_t n)
+{
+	size_t	i;
+
+	i = 0;
+	if (n <= 0)
+		return (0);
+	while (s1[i] == s2[i] && i < n)
+	{
+		if (!s1[i++])
+			return (0);
+	}
+	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
+}
