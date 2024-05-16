2882fed84b96c85229d338796600f2b239379c1e
Koldo Abasolo Iturregi <kabasolo@c3r6s2.42urduliz.com>
2024-05-16 19:32:30 +0200

    BuenoBonitoBarato

---
+++ src/libfta/ft_realloc.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_realloc.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/23 18:03:12 by kabasolo          #+#    #+#             */
+/*   Updated: 2024/04/23 18:35:47 by kabasolo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_realloc(void *ptr, size_t size)
+{
+	void	*new_ptr;
+
+	if (!size)
+		return (free(ptr), NULL);
+	new_ptr = malloc(size);
+	if (!new_ptr)
+		return (NULL);
+	if (ptr)
+	{
+		ft_memcpy(new_ptr, ptr, size);
+		free(ptr);
+	}
+	return (new_ptr);
+}
---
+++ src/libfta/mtrx_cpy.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   mtrx_cpy.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kabasolo <kabasolo@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/10 17:42:38 by kabasolo          #+#    #+#             */
+/*   Updated: 2024/04/10 19:20:25 by kabasolo         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	**mtrx_cpy(char **mtrx)
+{
+	int		i;
+	int		t;
+	char	**cpy;
+
+	i = mtrx_len(mtrx);
+	cpy = (char **)malloc((i + 1) * sizeof(char *));
+	if (!cpy)
+		return (NULL);
+	i = 0;
+	while (mtrx[i])
+	{
+		t = 0;
+		cpy[i] = (char *)malloc((ft_strlen(mtrx[i]) + 1) * sizeof(char ));
+		if (!cpy[i])
+			return (mtrx_free(cpy), NULL);
+		while (mtrx[i][t])
+		{
+			cpy[i][t] = mtrx[i][t];
+			t ++;
+		}
+		i ++;
+	}
+	return (cpy);
+}
